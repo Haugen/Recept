491eaf0738b672ac815f10500935404dfc36a101
Haugen <tobiashaugen@gmail.com>
2015-02-02 15:13:13 +0100

    Minor adjustment

--- app/templates/header.tpl.php
+++ app/templates/header.tpl.php
@@ -4,8 +4,8 @@
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <meta name="description" content="Twitterino">
-  <meta name="author" content="Tobias Haugen">
+  <meta name="description" content="Receptbanken">
+  <meta name="author" content="You">
 
   <title>Receptbanken</title>
 
@@ -24,14 +24,8 @@
           <span class="icon-bar"></span>
           <span class="icon-bar"></span>
         </button>
-        <a class="navbar-brand" href="#">Receptbanken</a>
+        <a class="navbar-brand" href="index.php">Receptbanken</a>
       </div>
-      <div id="navbar" class="navbar-collapse collapse">
-        <ul class="nav navbar-nav">
-          <li><a href="index.php">Home</a></li>
-          <li><a href="index.php?q=add-recipe-form">Lägg till recept</a></li>
-        </ul>
-      </div><!--/.nav-collapse -->
     </div>
   </nav>
   <div class="container">
